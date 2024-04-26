2fce00a6cee47ec3e9ecee0f553b72f793943db7
tarak <tarakaram.gasi@gmail.com>
2024-04-26 15:54:16 -0500

    Add project files.

---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Products;
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.GetProductInfo();
+            p1.DisplayProductInfo();
+            Console.WriteLine(p1.Addition(100, 200));
+            p1.ThisInterface();
+            Console.Read();
+        }
+    }
+}
---
+++ Products/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products
+{
+    public class Product:IProduct,Interface1
+    {
+        public void GetProductInfo()
+        {
+            Console.WriteLine("This is Product Info Method");
+        }
+        public void DisplayProductInfo()
+        {
+            Console.WriteLine("This is Display Product Info Method");
+        }
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public void ThisInterface()
+        {
+            Console.WriteLine("we are using Interface1 method");
+        }
+
+    }
+}
